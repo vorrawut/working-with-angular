24dc3619c8c7cc15f087f1a31c6e2371be9e97d5
Phatcharaphan ananpreechakun <phat.pan@gmail.com>
2019-11-15 23:52:25 +0700

    remove code unused

--- src/app/components/income/income.component.spec.ts
+++ src/app/components/income/income.component.spec.ts
@@ -1,13 +1,12 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-
-import { IncomeComponent } from "./income.component";
-import { ModalModule } from "ngx-bootstrap/modal";
-import { IncomeService } from "src/app/services/income/income.service";
-import { HttpClientTestingModule } from "@angular/common/http/testing";
-import { of } from "rxjs";
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IncomeComponent } from './income.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { IncomeService } from 'src/app/services/income/income.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 import { Income } from 'src/app/models/income/income';
 
-describe("IncomeComponent", () => {
+describe('IncomeComponent', () => {
   let component: IncomeComponent;
   let fixture: ComponentFixture<IncomeComponent>;
   let incomeService: IncomeService;
@@ -26,11 +25,11 @@ describe("IncomeComponent", () => {
     incomeService = TestBed.get(IncomeService);
   });
 
-  it("should create", () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  describe("getIncomeByUserId", () => {
+  describe('getIncomeByUserId', () => {
 
     let expectedIncomes: Income[];
 
@@ -38,39 +37,39 @@ describe("IncomeComponent", () => {
 
       expectedIncomes = [
         {
-          id: "1",
+          id: '1',
           incomeGroupId: 1,
-          incomeGroupName: "งานประจำ",
-          date: "14/11/2019",
+          incomeGroupName: 'งานประจำ',
+          date: '14/11/2019',
           amount: 150000
         },
         {
-          id: "2",
+          id: '2',
           incomeGroupId: 2,
-          incomeGroupName: "งานเสริม",
-          date: "14/10/2019",
+          incomeGroupName: 'งานเสริม',
+          date: '14/10/2019',
           amount: 150000
         },
         {
-          id: "3",
+          id: '3',
           incomeGroupId: 3,
-          incomeGroupName: "รายได้จากการลงทุน",
-          date: "14/10/2019",
+          incomeGroupName: 'รายได้จากการลงทุน',
+          date: '14/10/2019',
           amount: 150000
         }
       ];
 
-      spyOn(incomeService, "getIncomeByUserId").and.returnValue(of(expectedIncomes));
+      spyOn(incomeService, 'getIncomeByUserId').and.returnValue(of(expectedIncomes));
     });
 
-    it("should call getIncomeByUserId", () => {
+    it('should call getIncomeByUserId', () => {
 
       component.ngOnInit();
 
       expect(incomeService.getIncomeByUserId).toHaveBeenCalled();
     });
 
-    it("should set incomes from getIncomeByUserId", () => {
+    it('should set incomes from getIncomeByUserId', () => {
 
       component.ngOnInit();
 
--- src/app/components/income/income.component.ts
+++ src/app/components/income/income.component.ts
@@ -1,32 +1,30 @@
-import { Component, OnInit, TemplateRef } from "@angular/core";
-import { ChartType } from "chart.js";
-import { Label, MultiDataSet } from "ng2-charts";
-import { Income } from "src/app/models/income/income";
-import { BsModalRef, BsModalService } from "ngx-bootstrap/modal";
-import { HttpClient } from "@angular/common/http";
-import { IncomeService } from "src/app/services/income/income.service";
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Label, MultiDataSet } from 'ng2-charts';
+import { Income } from 'src/app/models/income/income';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { IncomeService } from 'src/app/services/income/income.service';
 
 @Component({
-  selector: "app-income",
-  templateUrl: "./income.component.html",
-  styleUrls: ["./income.component.scss"]
+  selector: 'app-income',
+  templateUrl: './income.component.html',
+  styleUrls: ['./income.component.scss']
 })
 export class IncomeComponent implements OnInit {
-  // Doughnut
   public doughnutChartLabels: Label[] = [
-    "งานประจำ",
-    "งานเสริม",
-    "รายได้จากการลงทุน"
+    'งานประจำ',
+    'งานเสริม',
+    'รายได้จากการลงทุน'
   ];
   public doughnutChartData: MultiDataSet = [[1000, 200, 300]];
-  public doughnutChartType: ChartType = "doughnut";
+  public doughnutChartType: ChartType = 'doughnut';
   modalRef: BsModalRef;
   income: Income[];
 
   constructor(
     private modalService: BsModalService,
     private incomeService: IncomeService
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.getIncomesByUserId();
