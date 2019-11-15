847ec42e32972322ba079abd51dbbcb2a77deec9
tumit <tumit@live.com>
2019-11-15 23:42:46 +0700

    refactor: income.component spec

--- src/app/components/income/income.component.spec.ts
+++ src/app/components/income/income.component.spec.ts
@@ -1,25 +1,22 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { IncomeComponent } from './income.component';
-import { ModalModule } from 'ngx-bootstrap/modal';
-import { IncomeService } from 'src/app/services/income/income.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { of } from 'rxjs';
+import { IncomeComponent } from "./income.component";
+import { ModalModule } from "ngx-bootstrap/modal";
+import { IncomeService } from "src/app/services/income/income.service";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { of } from "rxjs";
+import { Income } from 'src/app/models/income/income';
 
-describe('IncomeComponent', () => {
+describe("IncomeComponent", () => {
   let component: IncomeComponent;
   let fixture: ComponentFixture<IncomeComponent>;
   let incomeService: IncomeService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        ModalModule.forRoot(),
-        HttpClientTestingModule
-      ],
-      declarations: [ IncomeComponent ]
-    })
-    .compileComponents();
+      imports: [ModalModule.forRoot(), HttpClientTestingModule],
+      declarations: [IncomeComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -29,17 +26,55 @@ describe('IncomeComponent', () => {
     incomeService = TestBed.get(IncomeService);
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 
-  it('should call getIncomeByUserId', () => {
+  describe("getIncomeByUserId", () => {
 
-    const getIncomeByUserId = spyOn(incomeService, 'getIncomeByUserId').and.returnValue(of([]));
+    let expectedIncomes: Income[];
 
-    component.ngOnInit();
+    beforeEach(() => {
 
-    expect(getIncomeByUserId).toHaveBeenCalled();
+      expectedIncomes = [
+        {
+          id: "1",
+          incomeGroupId: 1,
+          incomeGroupName: "งานประจำ",
+          date: "14/11/2019",
+          amount: 150000
+        },
+        {
+          id: "2",
+          incomeGroupId: 2,
+          incomeGroupName: "งานเสริม",
+          date: "14/10/2019",
+          amount: 150000
+        },
+        {
+          id: "3",
+          incomeGroupId: 3,
+          incomeGroupName: "รายได้จากการลงทุน",
+          date: "14/10/2019",
+          amount: 150000
+        }
+      ];
+
+      spyOn(incomeService, "getIncomeByUserId").and.returnValue(of(expectedIncomes));
+    });
+
+    it("should call getIncomeByUserId", () => {
+
+      component.ngOnInit();
+
+      expect(incomeService.getIncomeByUserId).toHaveBeenCalled();
+    });
+
+    it("should set incomes from getIncomeByUserId", () => {
+
+      component.ngOnInit();
+
+      expect(component.income).toBe(expectedIncomes);
+    });
   });
-
 });
