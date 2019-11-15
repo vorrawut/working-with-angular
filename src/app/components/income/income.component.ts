0ba5e921258387ecf97192c7c525b6bd6c59abbf
tumit <tumit@live.com>
2019-11-15 23:29:10 +0700

    refactor: income.component

--- src/app/components/income/income.component.ts
+++ src/app/components/income/income.component.ts
@@ -1,37 +1,42 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
-import { ChartType } from 'chart.js';
-import { Label, MultiDataSet } from 'ng2-charts';
-import { Income } from 'src/app/models/income/income';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import { HttpClient } from '@angular/common/http';
-import { IncomeService } from 'src/app/services/income/income.service';
+import { Component, OnInit, TemplateRef } from "@angular/core";
+import { ChartType } from "chart.js";
+import { Label, MultiDataSet } from "ng2-charts";
+import { Income } from "src/app/models/income/income";
+import { BsModalRef, BsModalService } from "ngx-bootstrap/modal";
+import { HttpClient } from "@angular/common/http";
+import { IncomeService } from "src/app/services/income/income.service";
 
 @Component({
-  selector: 'app-income',
-  templateUrl: './income.component.html',
-  styleUrls: ['./income.component.scss']
+  selector: "app-income",
+  templateUrl: "./income.component.html",
+  styleUrls: ["./income.component.scss"]
 })
 export class IncomeComponent implements OnInit {
   // Doughnut
   public doughnutChartLabels: Label[] = [
-    'งานประจำ',
-    'งานเสริม',
-    'รายได้จากการลงทุน'
+    "งานประจำ",
+    "งานเสริม",
+    "รายได้จากการลงทุน"
   ];
   public doughnutChartData: MultiDataSet = [[1000, 200, 300]];
-  public doughnutChartType: ChartType = 'doughnut';
+  public doughnutChartType: ChartType = "doughnut";
   modalRef: BsModalRef;
   income: Income[];
 
-  constructor(private modalService: BsModalService, private incomeService: IncomeService) {}
+  constructor(
+    private modalService: BsModalService,
+    private incomeService: IncomeService
+  ) {}
 
   ngOnInit() {
+    this.getIncomesByUserId();
+  }
 
+  private getIncomesByUserId() {
     const income$ = this.incomeService.getIncomeByUserId();
     income$.subscribe(income => {
       this.income = income;
     });
-
   }
 
   openModal(template: TemplateRef<any>) {
